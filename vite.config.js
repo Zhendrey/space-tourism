c7eb1da669ee6db0b2d5e81f43e98cba0eecf204
Yevhen <132243427+Zhendrey@users.noreply.github.com>
2025-02-21 13:50:13 -0800

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: './', // This ensures that the paths are relative
+  base: '/space-tourism-deploy', // This ensures that the paths are relative
   build: {
     outDir: 'dist', // Output directory for the build
     target: 'esnext', // Ensure modern JavaScript features are supported
